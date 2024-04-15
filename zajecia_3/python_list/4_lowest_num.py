1ea633528f79ddd0259ec6844ea90e2454b44092
Sts-LMM <166924522+Sts-LMM@users.noreply.github.com>
2024-04-15 12:00:24 +0200

    Add files via upload

---
+++ zajecia_3/python_list/4_lowest_num.py
@@ -0,0 +1,15 @@
+def lowest_num_taker(all_nums): 
+    all_nums.sort()
+    final =all_nums[0]
+    return final
+
+
+all_nums = []
+while True:
+    try:
+        num_to_sum = int(input("Write num to get the lowest (enter letter to end list): "))
+        all_nums.append(num_to_sum)
+    except ValueError:
+        break
+print(all_nums)
+print('Highest num:', lowest_num_taker(all_nums))
