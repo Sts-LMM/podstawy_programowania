954af3a95b1674ce64049dc30461665d096511f2
Sts-LMM <166924522+Sts-LMM@users.noreply.github.com>
2024-04-15 11:25:34 +0200

    Add files via upload

---
+++ zajecia_3/python_list/2_list_multiplayer.py
@@ -0,0 +1,16 @@
+def multiplied_list(all_nums): 
+    final = 1
+    for i in range (0, len(all_nums)):
+        final *= all_nums[i]
+    return final
+
+
+all_nums = []
+while True:
+    try:
+        num_to_sum = int(input("Write num to multiply (enter letter to end list): "))
+        all_nums.append(num_to_sum)
+    except ValueError:
+        break
+print(all_nums[:len(all_nums)])
+print('Final:', multiplied_list(all_nums))
