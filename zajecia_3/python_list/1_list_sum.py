77c191b735e01a08b24d960481d682a9297a8133
Sts-LMM <166924522+Sts-LMM@users.noreply.github.com>
2024-04-15 11:08:50 +0200

    Add files via upload

---
+++ zajecia_3/python_list/1_list_sum.py
@@ -0,0 +1,14 @@
+def sum_list(all_nums): 
+    final = sum(all_nums)
+    return final
+
+
+all_nums = []
+while True:
+    try:
+        num_to_sum = int(input("Write num to sum (enter letter to end list): "))
+        all_nums.append(num_to_sum)
+    except ValueError:
+        break
+print(all_nums[:len(all_nums)])
+print('Sum:' + sum_list(all_nums))
