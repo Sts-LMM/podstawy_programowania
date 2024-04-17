239c04c666fdd0c4ba077937b94644e2927f42ff
Sts-LMM <166924522+Sts-LMM@users.noreply.github.com>
2024-04-17 20:29:25 +0200

    Add files via upload

---
+++ zajecia_3/python_dictionary/7_power_from_1_to_15.py
@@ -0,0 +1,9 @@
+def the_power_of(random_dictionary):
+     pow2 = {}
+     for i in range (1,random_dictionary+1):
+          pow2.update({i:(i*i)})
+     print(pow2)
+     return True
+
+how_many_to_the_power_of_two = 15
+the_power_of(how_many_to_the_power_of_two)
