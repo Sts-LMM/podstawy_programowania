505e393b65f9721b0bb0201abd804774e5b414ee
Sts-LMM <166924522+Sts-LMM@users.noreply.github.com>
2024-04-24 19:26:13 +0200

    Delete zajecia_3/16_only_even.py

--- zajecia_3/16_only_even.py
+++
@@ -1,5 +0,0 @@
-def are_all_digits_even(number):
-    return all(int(digit) % 2 == 0 for digit in str(number))
-
-even_digit_numbers = [str(number) for number in range(100, 401) if are_all_digits_even(number)]
-print(",".join(even_digit_numbers))
